fd8bd5996dae743c35734c50130dac30c1720d53
Juzoo-var <juzoo.contact@gmail.com>
2020-05-06 11:34:45 +0200

    SimplyJava

--- src/fr/juzoo/simplyjava/func/SFrame.java
+++ src/fr/juzoo/simplyjava/func/SFrame.java
@@ -1,35 +1,60 @@
 package fr.juzoo.simplyjava.func;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SFrame {
+    //get current window title
     public String getTitle() {
         return title;
     }
-
+    //set new window title
     public void setTitle(String title) {
         this.title = title;
     }
-
+    //get current width(size)
     public int getWidth() {
         return width;
     }
-
+    //set width(size)
     public void setWidth(int width) {
         this.width = width;
     }
-
-    public int getWeight() {
+    //get current height(size)
+    public int getHeight() {
         return height;
     }
-
+    //set height(size)
     public void setHeight(int weight) {
         this.height = weight;
     }
 
+    //default window name
     private String title = "SFrame";
+    //default width
     private int width = 1080;
+    //default height
     private int height = 720;
+
+    public JFrame getSFrame() {
+        return SFrame;
+    }
+
+    public void setSFrame(JFrame SFrame) {
+        this.SFrame = SFrame;
+    }
+
+    private JFrame SFrame = new JFrame();
+
+    public Color getBackgroundColor() {
+        return color;
+    }
+
+    public void setBackgroundColor(Color color) {
+        this.color = color;
+    }
+
+    private Color color = Color.WHITE;
 
     public boolean isVisible() {
         return visible;
@@ -44,9 +69,10 @@ public class SFrame {
     public SFrame() {
     }
     public void openFrame(){
-        JFrame SFrame = new JFrame();
-        SFrame.setSize(this.getWidth(), this.getWeight());
+
+        SFrame.setSize(this.getWidth(), this.getHeight());
         SFrame.setTitle(this.getTitle());
+        SFrame.getContentPane().setBackground(this.getBackgroundColor());
         SFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         SFrame.setVisible(this.isVisible());
     }
